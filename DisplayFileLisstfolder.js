57298dfaaa0970f42ad54afcb9524fbe9b0b1896
shahzaibhaider746 <devshahzaib63@gmail.com>
2025-01-12 03:29:25 +0500

    Asynchronous Programming Language

--- DisplayFileLisstfolder.js
+++ DisplayFileLisstfolder.js
@@ -1,13 +1,34 @@
 const fs = require('fs')
 const path = require('path')
 const dirPath = path.join(__dirname , "files")
-console.warn(dirPath)
-// fs.writeFileSync("hello.txt", "This is hello file")
+const filePath = `${dirPath}/hello.txt`
+// console.warn(filePath)
+// fs.writeFileSync(`${dirPath}/hello.txt , "hello "`)
 
 // for( i = 0; i< 5 ; i++ ){
 //     fs.writeFileSync( dirPath+"/hello"+i+".txt", "This is hello file")
 // }
 
-fs.readdir(dirPath, (err, files)=>{
-console.log(files , 'my files')
-})
+// fs.readdir(dirPath, (err, files)=>{
+// // console.log(files , 'my files')
+// files.forEach((item)=>{
+// console.log(item)
+// })
+// })
+
+
+
+//To add some thing
+// fs.appendFile(filePath , " my name is shahzaib", (err)=>{
+  
+//     if(!err)console.log("Updated true")
+
+// })
+
+//To rename of file 
+// fs.rename(filePath, `${dirPath}/Details.txt`, (err)=>{
+// if (!err)console.log("updateFile Name True")
+// })
+
+//TO delete the file 
+fs.unlinkSync(`${dirPath}/Details.txt`)
