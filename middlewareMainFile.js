6876af3bb90ddb4b0902d8bc055707a7c082058a
shahzaibhaider746 <devshahzaib63@gmail.com>
2025-01-16 16:25:43 +0500

    Group MiddlleWare

--- middlewareMainFile.js
+++ middlewareMainFile.js
@@ -1,20 +1,25 @@
 const express = require('express')
 const app = express();
 const reqFilter = require('./middleWare')
-
+const route =  express.Router()
 
 
 // app.use(reqFilter)
+
+route.use(reqFilter)
 app.get("/",(req, res)=>{
     res.send("<h1>Hello to welcome Screen </h1>")
 })
 
-app.get("/user",reqFilter,(req, res)=>{
+route.get("/user",(req, res)=>{
     res.send("<h1>Hello to user screen  </h1>")
 })
 
-app.get("/about",(req, res)=>{
+route.get("/about",(req, res)=>{
     res.send("<h1>Hello to about screen  </h1>")
 })
-
+route.get("/contant",(req, res)=>{
+    res.send("<h1>Hello to contant screen  </h1>")
+})
+app.use('/', route)
 app.listen(5000)
